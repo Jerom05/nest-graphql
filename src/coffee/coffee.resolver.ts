05d80cf87d1715d67e7b740aed1430bffe6971de
jerom ghagra <jeromghagra@gmail.com>
2025-06-30 22:12:30 +0600

    feat: implement CoffeeResolver with queries and mutations for coffee management

--- src/coffee/coffee.resolver.ts
+++ src/coffee/coffee.resolver.ts
@@ -1,4 +1,24 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Int, Query, Resolver, Mutation } from '@nestjs/graphql';
+import { Coffee } from './coffee.entity';
+import { CreateCoffeeInput } from './coffee.input';
+import { CoffeeService } from './coffee.service';
 
-@Resolver()
-export class CoffeeResolver {}
+@Resolver(() => Coffee)
+export class CoffeeResolver {
+  constructor(private coffeeService: CoffeeService) {}
+
+  @Query(() => [Coffee])
+  coffees(): Coffee[] {
+    return this.coffeeService.findAll();
+  }
+
+  @Query(() => Coffee, { nullable: true })
+  coffee(@Args('id', { type: () => Int }) id: number) {
+    return this.coffeeService.findOne(id);
+  }
+
+  @Mutation(() => Coffee)
+  createCoffee(@Args('createCoffeeInput') input: CreateCoffeeInput) {
+    return this.coffeeService.create(input);
+  }
+}
